89df3938e9c4419c4b05edda485c11c8710d1cb7
KaitouJap <kaitoujap@gmail.com>
2024-11-20 11:19:56 +0100

    genericdatabase

---
+++ GenerikusAdatbazisKezelo/DatabaseManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenerikusAdatbazisKezelo
+{
+    public class DatabaseManager<V> where V : IRecord
+    {
+        Dictionary<int, V> database = new Dictionary<int, V>();
+
+        public void AddRecord(V record)
+        {
+            database.Add(record.Id,record);
+        }
+
+        public void RemoveRecord(int recordId)
+        {
+            
+            if (!database.Remove(recordId))
+            {
+                throw new KeyNotFoundException();
+            }
+        }
+
+        public V GetRecord(int recordId)
+        {
+            if(database.TryGetValue(recordId, out V value))
+            {
+                return value;
+            }
+            else
+            {
+                throw new KeyNotFoundException();
+            }
+        }
+
+        public void PrintDatabase()
+        {
+            foreach (KeyValuePair<int, V> record in database)
+            {
+                Console.WriteLine(record.Value.ToString());
+            }
+        }
+    }
+}
---
+++ GenerikusAdatbazisKezelo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenerikusAdatbazisKezelo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DatabaseManager<Student> database1 = new DatabaseManager<Student>();
+            
+            Student student1 = new Student();
+            Student student2 = new Student();
+            Student student3 = new Student();
+            student1.Id = 0;
+            student2.Id = 1;
+            student3.Id = 2;
+            student1.Age = 18;
+            student2.Age = 17;
+            student3.Age = 23;
+            student1.Name = "Janos";
+            student2.Name = "Pista";
+            student3.Name = "Gergo";
+
+            database1.AddRecord(student1);
+            database1.AddRecord(student2);
+            database1.AddRecord(student3);
+
+            database1.PrintDatabase();
+
+            try
+            {
+                Console.WriteLine(database1.GetRecord(3));
+                database1.RemoveRecord(3);
+                database1.PrintDatabase();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
